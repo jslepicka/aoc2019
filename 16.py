2b4b8109d956b75dd5b1631b8fe70a900db0eb5e
James Slepicka <james@slepicka.net>
2019-12-16 13:43:39 -0600

    day 16

--- 16.py
+++ 16.py
@@ -4,26 +4,42 @@ def read_input():
         return file.readline().rstrip()
 
 def part1(input_signal):
-    input_signal = "19617804207202209144916044189917"
+    input_signal = [int(x) for x in input_signal]
     input_len = len(input_signal)
-    print("input signal length: %d" % input_len)
     pattern = [0, 1, 0, -1]
     total = input_signal
     for phase in range(100):
         input_signal = total
-        total = ""
+        total = []
         for offset in range(len(input_signal)):
             digit = offset
             sum = 0
             for i in input_signal[digit:]:
                 coeff_offset = (((digit+1) // (offset+1)) % 4)
-                #print("coeff: %d" % coeff_offset)
-                sum += int(i) * pattern[coeff_offset]
+                sum += i * pattern[coeff_offset]
                 digit += 1
-            total += str(abs(sum) % 10)
-        #print("%d" % phase)
-    return total[:8]
+            total.append(abs(sum) % 10)
+    return "".join([str(i) for i in total[:8]])
+
+def part2(input_signal):
+    #at the halfway point, all of the coefficients change to 1
+    #the offset is in the second half
+    #calculate sum of second half of digits
+    input_signal *= 10000
+    input_len = len(input_signal)
+    offset = int(input_signal[:7])
+    phase_signal = [int(x) for x in input_signal[offset:]]
+    for phase in range(100):
+        phase_signal_sum = sum(phase_signal)
+        for i in range(0, len(phase_signal)):
+            ps = phase_signal[i]
+            phase_signal[i] = phase_signal_sum % 10
+            phase_signal_sum -= ps
+    return("".join([str(i) for i in phase_signal[:8]]))
 
 input_signal = read_input()
-print(part1(input_signal))
-#print(part1(input_signal * 10000))
+print(part1(read_input()))
+print(part2(read_input()))
+
+
+
